91ba4880a7bdae70e54a1cf9c7683c99ebbde072
Michal Kozik <michal.kozikowski99@gmail.com>
2021-03-16 19:03:20 +0100

    Initial commit

---
+++ src/main/java/com/algorithms/Main.java
@@ -0,0 +1,12 @@
+package com.algorithms;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("start");
+    }
+
+    public static double max(double a, double b) {
+        return a>b ? a : b;
+    }
+}
---
+++ src/test/java/com/algorithms/RailFenceTest.java
@@ -0,0 +1,11 @@
+package com.algorithms;
+
+import org.junit.jupiter.api.Test;
+
+public class RailFenceTest {
+
+    @Test
+    public void test1() {
+        assert Main.max(2.0,1.5) == 2.0;
+    }
+}
