7e07dfdebee1f356095d1c3fb289b365ae12bc9e
Michal Kozik <michal.kozikowski99@gmail.com>
2021-03-16 19:08:08 +0100

    Dodanie szyfru płotkowego.

--- src/main/java/com/algorithms/Main.java
+++ src/main/java/com/algorithms/Main.java
@@ -3,7 +3,15 @@ package com.algorithms;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("start");
+
+        int k = 3;
+        String word = "CRYPTOGRAPHY";
+
+        RailFence railFence = new RailFence();
+        String codedMessage = railFence.code(word, k);
+        System.out.println(codedMessage);
+        String decodedMessage = railFence.decode(codedMessage, k);
+        System.out.println(decodedMessage);
     }
 
     public static double max(double a, double b) {
--- src/main/java/com/algorithms/RailFence.java
+++ src/main/java/com/algorithms/RailFence.java
@@ -1,4 +1,93 @@
 package com.algorithms;
 
 public class RailFence {
+
+    public String code(String word, int key) {
+        char[][] fence = new char[word.length()][key];
+        boolean down = false;
+        int row = 0;
+        int col = 0;
+
+        // Tworzenie plotka.
+        for (int i = 0; i < word.length(); i++) {
+            // Odbijanie sie od podlogi i sufitu.
+            if (row == 0 || row == key - 1)
+                down = !down;
+            // Wstawianie litery.
+            fence[col][row] = word.charAt(i);
+            // Znalezienie nowej pozycji
+            col++;
+            if (down)
+                row++;
+            else
+                row--;
+        }
+
+        // Odczytywanie plotka
+        String result = "";
+        for (int j = 0; j < key; j++) {
+            for (int i = 0; i < word.length(); i++) {
+                // Jezeli znaleziono litere
+                if (fence[i][j] != 0) {
+                    result += fence[i][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    public String decode(String word, int key) {
+        char[][] fence = new char[word.length()][key];
+        boolean down = false;
+        int row = 0;
+        int col = 0;
+
+        // Tworzenie plotka.
+        for (int i = 0; i < word.length(); i++) {
+            // Odbijanie sie od sufitu i podlogi.
+            if (row == 0 || row == key - 1)
+                down = !down;
+            // Zaznaczenie miejsca wystapienia litery.
+            fence[col][row] = '*';
+            // Znalezienie nastepnego miejsca.
+            col++;
+            if (down)
+                row++;
+            else
+                row--;
+        }
+
+        String result = "";
+        int counter = 0;
+        // Zastapienie gwiazdek literami.
+        for (int j = 0; j < key; j++) {
+            for (int i = 0; i < word.length(); i++) {
+                // Podmienianie gwiazdek na litery z szyfru idac od gory do dolu
+                // z lewej do prawej.
+                if (fence[i][j] == '*') {
+                    fence[i][j] = word.charAt(counter);
+                    counter++;
+                }
+            }
+        }
+
+        down = false;
+        col = 0;
+        row = 0;
+        // Odczytanie hasla.
+        for (int i = 0; i < word.length(); i++) {
+            // Odbijanie sie.
+            if (row == 0 || row == key - 1)
+                down = !down;
+            // Przypisanie.
+            result += fence[col][row];
+            // Przejscie dalej.
+            col++;
+            if (down)
+                row++;
+            else
+                row--;
+        }
+        return result;
+    }
 }
