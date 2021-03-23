c0753661629683fa7dc92b1e8df9f9b5933e8ef4
Michal Kozik <michal.kozikowski99@gmail.com>
2021-03-23 19:51:28 +0100

    Przestawienie macierzowe A - dodanie dekodowania.

--- src/main/java/com/algorithms/Main.java
+++ src/main/java/com/algorithms/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int d = 5;
-        String word = "CRYPTOGRAPHY";
-        int [] key = {3, 4, 1, 5, 2};
+        int d = 5;                      // dlugosc klucza.
+        String word = "YPCTRRAOPGHY";   // slowo.
+        int [] key = {3, 4, 1, 5, 2};   // klucz.
 
         MatrixCipherA matrixCipherA = new MatrixCipherA();
-        String encodedWord = matrixCipherA.encode(word, d, key);
-        System.out.println(encodedWord);
+//        String encodedWord = matrixCipherA.encode(word, d, key);
+//        System.out.println(encodedWord);
+        String decodedWord = matrixCipherA.decode(word, d, key);
+        System.out.println(decodedWord);
     }
 
     public static double max(double a, double b) {
--- src/main/java/com/algorithms/MatrixCipherA.java
+++ src/main/java/com/algorithms/MatrixCipherA.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class MatrixCipherA {
 
     public String encode(String word, int d, int [] key) {
+        // BRAK SPRAWDZENIA CZY WPISYWANE DANE SA POPRAWNE.
         char[][] matrix = new char[word.length()/d + 1][d];
         int iterator = 0;
 
@@ -27,6 +28,40 @@ public class MatrixCipherA {
         for (int j = 0; j < word.length()/d + 1; j++) {
             for (int i = 0; i < d; i++) {
                 result += matrix[j][key[i]-1];
+            }
+        }
+        result = result.replace("0", "");
+        return result;
+    }
+
+    public String decode(String word, int d, int [] key) {
+        //BRAK SPRAWDZENIA CZY WPISYWANE DANE SA POPRAWNE.
+        char[][] matrix = new char[word.length()/d + 1][d];
+        int iterator = 0;
+
+        // Ilosc komorek - ilosc liter = puste komorki.
+        int space = ((word.length()/d + 1) * d) - word.length();
+        // Wpisanie zer do pustych komorek od konca macierzy.
+        for (int i = d-1; space > 0; i--) {
+            matrix[word.length()/d][i] = '0';
+            space--;
+        }
+
+        // Wpisywanie liter na swoje miejsca w macierzy.
+        for (int j = 0; j < word.length()/d + 1; j++) {
+            for (int i = 0; i < d; i++) {
+                if (matrix[j][key[i]-1] != '0') {
+                    matrix[j][key[i]-1] = word.charAt(iterator);
+                    iterator++;
+                }
+            }
+        }
+
+        // Oczytanie liter w poprawnej kolejnosci
+        String result = "";
+        for (int j = 0; j < word.length()/d + 1; j++) {
+            for (int i = 0; i < d; i++) {
+                result += matrix[j][i];
             }
         }
         result = result.replace("0", "");
