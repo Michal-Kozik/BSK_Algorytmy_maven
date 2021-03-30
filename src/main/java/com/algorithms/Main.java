325d5d80e2d8f6bc511924913cb3892818d6edb9
Michal Kozik <michal.kozikowski99@gmail.com>
2021-03-30 19:35:05 +0200

    Przestawienie macierzowe B - dodanie dekodowania.

--- src/main/java/com/algorithms/Main.java
+++ src/main/java/com/algorithms/Main.java
@@ -6,10 +6,13 @@ public class Main {
 
         String word = "HEREISASECRETMESSAGEENCIPHEREDBYTRANSPOSITION";   // slowo.
         String key = "CONVENIENCE";   // klucz.
+        String word2 = "HECRNCEYIISEPSGDIRNTOAAESRMPNSSROEEBTETIAEEHS";
 
         MatrixCipherB matrixCipherB = new MatrixCipherB();
         String encodedWord = matrixCipherB.encode(word, key);
         System.out.println(encodedWord);
+        String decodedWord = matrixCipherB.decode(word2, key);
+        System.out.println(decodedWord);
     }
 
     public static double max(double a, double b) {
--- src/main/java/com/algorithms/MatrixCipherB.java
+++ src/main/java/com/algorithms/MatrixCipherB.java
@@ -23,7 +23,7 @@ public class MatrixCipherB {
             }
         }
 
-        // Wstawienie odpowiednich liczb do klucza
+        // Wstawienie odpowiednich liczb do klucza.
         Integer[] keyNumbers = new Integer[cols];
         int iteratorKey = 1;
         for (int i = 65; i <= 90; i++) {
@@ -36,7 +36,7 @@ public class MatrixCipherB {
             }
         }
 
-        // Odczytanie zaszyfrowanej wiadomosci
+        // Odczytanie zaszyfrowanej wiadomosci.
         String result = "";
         List<Integer> keyNumbersList = Arrays.asList(keyNumbers);
         for (int j = 0; j < cols; j++) {
@@ -46,6 +46,59 @@ public class MatrixCipherB {
                 }
             }
         }
+
+        return result;
+    }
+
+    public String decode(String word, String key) {
+        int rows = word.length()/key.length() + 1;
+        int cols = key.length();
+        char[][] matrix = new char[rows][cols];
+        int iterator = 0;
+
+        // Wstawienie zer do pustych komorek.
+        int spaces = rows * cols - word.length();
+        int index = cols - 1;
+        while (spaces > 0) {
+            matrix[rows - 1][index] = '0';
+            spaces--;
+            index--;
+        }
+
+        // Wstawienie odpowiednich liczb do klucza.
+        Integer[] keyNumbers = new Integer[cols];
+        int iteratorKey = 1;
+        for (int i = 65; i <= 90; i++) {
+            for (int j = 0; j < key.length(); j++) {
+                // jesli taki sam znak
+                if (i == (int)key.charAt(j)) {
+                    keyNumbers[j] = iteratorKey;
+                    iteratorKey++;
+                }
+            }
+        }
+
+        // Wstawienie liter do macierzy.
+        List<Integer> keyNumbersList = Arrays.asList(keyNumbers);
+        for (int j = 0; j < cols; j++) {
+            for (int i = 0; i < rows; i++) {
+                if (matrix[i][keyNumbersList.indexOf(j+1)] != '0') {
+                    matrix[i][keyNumbersList.indexOf(j+1)] = word.charAt(iterator);
+                    iterator++;
+                }
+            }
+        }
+
+        // Odczytanie liter z macierzy w poprawnej kolejnosci.
+        String result = "";
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result += matrix[i][j];
+            }
+        }
+
+        // Usuniecie zer.
+        result = result.replace("0", "");
 
         return result;
     }
